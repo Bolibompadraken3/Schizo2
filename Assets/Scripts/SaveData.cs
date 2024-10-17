9685f7ef63cd18c47fac8dc05d651b32fc67f869
Adonai <adonai.osterberg@elev.ga.lbs.se>
2024-10-17 10:24:57 +0200

    Shop
    ingen return knapp som skickar tillbaka dig till spelet än.

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SaveData", menuName = "Scriptable Objects/DataStorage")]
 public class SaveData : ScriptableObject
 {
+    //Adonai Österberg
     [SerializeField] public int insanityPoints; //Saves your current points.
     [SerializeField] public int clickerUpgradeLevel; //Saves the amount of upgrades you’ve made.
+    [SerializeField] public int autoClickerUpgrade; //Saves your autoclicker upgrades.
     [SerializeField] public bool autoClicker; //Tells and saves whether or not you’ve purchased the auto clicker upgrade.
 }
